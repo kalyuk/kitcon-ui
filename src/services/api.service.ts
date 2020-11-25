2d9a5916641dfaf97d6e65562e5a092616dee740
Dmitry Kalyuk <kalyukdo@gmail.com>
2020-11-25 15:42:59 +0300

    fix

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -8,10 +8,21 @@ export class ApiService {
         return this.request('GET', url, { query });
     }
 
+    async post(url: string, body: any, query: {} = {}) {
+        return this.request('POST', url, { body, query })
+    }
+
     async request(method: 'GET' | 'POST' | 'PUT' | 'DELETE', url: string, options: any = {}) {
         const endpoint = `/api${url}${options.query ? '?' + qs.stringify(options.query) : ''}`;
+
+        const headers: any = {
+            'content-type': 'application/json'
+        };
+
         const response = await fetch(endpoint, {
-            method
+            method,
+            headers,
+            body: options.body ? JSON.stringify(options.body) : null
         });
 
         const result = await response.json();
